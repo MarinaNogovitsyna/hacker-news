c37447bbd43491d7b48dd0fe67e7eb93a4ee66c4
Marina <marina.nogovitsyna.98@mail.ru>
2024-07-24 06:55:00 +0300

    Added @media

--- src/components/MainPage/StoryItem/StoryItem.tsx
+++ src/components/MainPage/StoryItem/StoryItem.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { Story } from '../../../types'
-import styles from './StoryItem.module.css'
+import React from "react";
+import { Story } from "../../../types";
+import styles from "./StoryItem.module.css";
 import { FaComments } from "react-icons/fa";
-import { getDate, getTime } from '../../../utils/getDate';
-import { Author } from '../../Author/Author';
+import { getDate, getTime } from "../../../utils/getDate";
+import { Author } from "../../Author/Author";
 
 interface StoryItemProps {
-    story: Story;
-    handleClick: () => void
+  story: Story;
+  handleClick: () => void;
 }
 
-export const StoryItem: React.FC<StoryItemProps> = ({story, handleClick}) => {
-
+export const StoryItem: React.FC<StoryItemProps> = ({ story, handleClick }) => {
   const getRatingColor = () => {
-    const color = story.score < 20 ? styles.rating_bad : story.score < 100 ? styles.rating_normal : styles.rating_good
-    return styles.rating + ' ' + color
-  }
+    const color =
+      story.score < 20
+        ? styles.rating_bad
+        : story.score < 100
+        ? styles.rating_normal
+        : styles.rating_good;
+    return styles.rating + " " + color;
+  };
 
   return (
     <div className={styles.story} onClick={handleClick}>
@@ -23,14 +27,16 @@ export const StoryItem: React.FC<StoryItemProps> = ({story, handleClick}) => {
         <span className={styles.story_title}>{story.title}</span>
         <span className={getRatingColor()}>{story.score}</span>
       </div>
-      <Author name={story.by}/>
+      <Author name={story.by} />
       <div className={styles.story_comments_and_date}>
         <div className={styles.story_comments}>
-          <FaComments color='gray'/>
-          <span>{story.kids ? story.kids.length : '0'}</span>
+          <FaComments color="gray" />
+          <span>{story.kids ? story.kids.length : "0"}</span>
         </div>
-        <span className={styles.story_date}>{getDate(story.time) + ' ' + getTime(story.time)}</span>
+        <span className={styles.story_date}>
+          {getDate(story.time) + " " + getTime(story.time)}
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
